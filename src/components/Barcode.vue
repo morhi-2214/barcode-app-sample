47e697ce56509477fbd33d6350318b98a2c5503f
Taiga Ito <t.ito@sonicmoov.com>
2021-09-15 01:43:52 +0900

    #1 qrcode reader sample

---
+++ src/components/Barcode.vue
@@ -0,0 +1,12 @@
+<template>
+  <qrcode-stream @decode="$emit('decode', $event)" />
+</template>
+<script>
+import { QrcodeStream } from "vue-qrcode-reader";
+export default {
+  name: "Barcode",
+  components: {
+    QrcodeStream,
+  },
+};
+</script>
